e6e557505badf84831de3401a7318b9dcd661e4e
lumachroma <b.blakk@gmail.com>
2021-07-01 12:37:12 +0800

    Create Likes schema

---
+++ models/schemas/likes.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose'
+
+const LikesSchema = new mongoose.Schema({
+    key: { type: String, required: true, index: { unique: true } },
+    count: { type: Number, required: true },
+})
+
+export default LikesSchema
