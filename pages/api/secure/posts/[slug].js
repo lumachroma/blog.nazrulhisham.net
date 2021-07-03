81fb46f4a0a08e9e2b64ff8ce35d877e888cebc7
lumachroma <b.blakk@gmail.com>
2021-07-03 13:55:51 +0800

    Create API endpoint to update Blog Post by post slug (likes and likesCount is protected/unmodifiable)

--- pages/api/secure/posts/[slug].js
+++ pages/api/secure/posts/[slug].js
@@ -21,4 +21,26 @@ handler.get(async (req, res) => {
   }
 })
 
+handler.put(async (req, res) => {
+  const { query: { slug }, body } = req
+
+  let updated = { ...body }
+  delete updated.likes
+  delete updated.likesCount
+
+  try {
+    const editedProduct = await BlogPost.findOneAndUpdate({ slug }, updated, {
+      new: true,
+      runValidators: true,
+    }).lean()
+    if (!editedProduct) {
+      return res.status(500).json({ success: false })
+    }
+    res.status(200).json({ success: true, data: editedProduct })
+  } catch (error) {
+    // res.status(409).json({ success: false })
+    res.status(409).json({ success: false, error })
+  }
+})
+
 export default handler
