af5f6a175d204bb808517283bb74e6eb1258b92c
lumachroma <b.blakk@gmail.com>
2021-07-04 14:26:48 +0800

    Create Secure Page Shell component (to enclose and protect secured pages)

--- components/PageSecuredShell.js
+++ components/PageSecuredShell.js
@@ -1,7 +1,18 @@
+import { useAuth } from '../lib/auth'
 import PageShell from './PageShell'
+import AccessDenied from './AccessDenied'
 
 const PageSecuredShell = ({ name, children }) => {
-  //TODO: secure routing
+  const { user } = useAuth()
+
+  if (!user) {
+    return (
+      <PageShell name="Access Denied">
+        <AccessDenied />
+      </PageShell>
+    )
+  }
+
   return (
     <PageShell name={name}>
       {children}
