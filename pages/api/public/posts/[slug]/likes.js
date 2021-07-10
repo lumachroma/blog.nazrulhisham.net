09fc36048a4a5556cc5eac2b02bbbca3efb3daee
lumachroma <b.blakk@gmail.com>
2021-07-11 00:48:50 +0800

    Create public post likes API endpoint to get current total likes & individual visitor's likes, at the same time, create new likes entry for first time visitor

---
+++ pages/api/public/posts/[slug]/likes.js
@@ -0,0 +1,43 @@
+import nextConnect from 'next-connect'
+import { all } from '../../../../../middlewares'
+import BlogPost from '../../../../../models/BlogPost'
+
+const handler = nextConnect()
+
+handler.use(all)
+
+handler.get(async (req, res) => {
+  const { query: { slug } } = req
+  const { socket: { remoteAddress } } = req
+
+  try {
+    const post = await BlogPost.findOne(
+      { slug, isActive: true },
+      { likesCount: 1, likes: { $elemMatch: { key: remoteAddress } }, _id: 0 },
+    ).lean()
+    if (post) {
+      if (!post.likes) {
+        const updatedPost = await BlogPost.findOneAndUpdate(
+          { slug, isActive: true },
+          { $push: { likes: { key: remoteAddress, count: 0 } } },
+          {
+            new: true,
+            runValidators: true,
+            fields: { likesCount: 1, likes: { $elemMatch: { key: remoteAddress } }, _id: 0 },
+          }
+        ).lean()
+        if (updatedPost) {
+          return res.status(200).json({ success: true, data: updatedPost })
+        }
+      }
+      res.status(200).json({ success: true, data: post })
+    } else {
+      res.status(404).json({ success: false })
+    }
+  } catch (error) {
+    // res.status(409).json({ success: false })
+    res.status(409).json({ success: false, error })
+  }
+})
+
+export default handler
