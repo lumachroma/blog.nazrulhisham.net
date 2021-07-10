26b5ae7e99d8bd40b807e5aba3119ec07708493b
lumachroma <b.blakk@gmail.com>
2021-07-11 01:09:07 +0800

    Show total Hits and total Likes on Blog Post page

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,50 +1,66 @@
 
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { MDXRemote } from 'next-mdx-remote'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import PageShell from '../../components/PageShell'
+import Hits from '../../components/Hits'
+import Likes from '../../components/Likes'
 
 const components = {
   blockquote: (props) => <blockquote className="lead fw-lighter lh-sm bg-light m-2 p-2" {...props} />,
 }
 
 const Post = ({ source, frontMatter }) => {
+  const router = useRouter()
+  const { id } = router.query
+
   return (
-    <article>
-      <h1 className="display-4">{frontMatter.title}</h1>
-      <p className="lead fw-lighter">
-        <small><mark>{frontMatter.date}</mark></small>
-        {' '}
-        {frontMatter.description}
-      </p>
-      {frontMatter.tags
-        && Array.isArray(frontMatter.tags)
-        && frontMatter.tags.length > 0
-        && <p>
-          {frontMatter.tags.map((tag, index) => {
-            return (
-              <span key={index}>
-                <span className="badge bg-light text-secondary">{tag}</span>
-                {' '}
-              </span>
-            )
-          })}
-        </p>
-      }
-      <p>
-        <Link href="/blog">
-          <a className="text-decoration-none">Back</a>
-        </Link>
-      </p>
-      <div>
-        <MDXRemote {...source} components={components} />
+    <>
+      <div className="row">
+        <div className=" col-10 col-md-11">
+          <article>
+            <h1 className="display-4">{frontMatter.title}</h1>
+            <p className="lead fw-lighter">
+              <small><mark>{frontMatter.date}</mark></small>
+              {' '}
+              {frontMatter.description}
+            </p>
+            {frontMatter.tags
+              && Array.isArray(frontMatter.tags)
+              && frontMatter.tags.length > 0
+              && <p>
+                {frontMatter.tags.map((tag, index) => {
+                  return (
+                    <span key={index}>
+                      <span className="badge bg-light text-secondary">{tag}</span>
+                      {' '}
+                    </span>
+                  )
+                })}
+              </p>
+            }
+            <p>
+              <Link href="/blog">
+                <a className="text-decoration-none">Back</a>
+              </Link>
+            </p>
+            <div>
+              <MDXRemote {...source} components={components} />
+            </div>
+            <p>
+              <Link href="/blog">
+                <a className="text-decoration-none">Back</a>
+              </Link>
+            </p>
+          </article>
+        </div>
+        <div className=" col-2 col-md-1">
+          <Likes postSlug={id} />
+          <Hits postSlug={id} />
+        </div>
       </div>
-      <p>
-        <Link href="/blog">
-          <a className="text-decoration-none">Back</a>
-        </Link>
-      </p>
-    </article>
+    </>
   )
 }
 
