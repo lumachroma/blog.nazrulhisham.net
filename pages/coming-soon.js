9dbfd66f090efd1b46aadfc2eb513a30714dc76f
lumachroma <b.blakk@gmail.com>
2021-07-25 01:54:37 +0800

    Create Coming Soon Page

---
+++ pages/coming-soon.js
@@ -0,0 +1,27 @@
+import PageShell from '../components/PageShell'
+
+const ComingSoon = () => {
+  return (
+    <>
+      <div className="py-3 px-3 bg-light border rounded-3">
+        <h1 className="display-4">
+          <i className="bi bi-cone-striped" style={{ color: "crimson" }} />{' '}
+          Coming Soon
+        </h1>
+        {/* <h2><i className="bi bi-cone-striped" /></h2> */}
+        <p className="lead">
+          Page is under construction. Stay tuned!{' '}
+          ⏰
+        </p>
+      </div>
+    </>
+  )
+}
+
+const ComingSoonPage = () => (
+  <PageShell name={"About"}>
+    <ComingSoon />
+  </PageShell>
+)
+
+export default ComingSoonPage
